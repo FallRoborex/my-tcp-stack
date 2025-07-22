5ff1e0d02012125657b5d90cc5b7ec7186646c8f
Roberto Ramirez <robertoramirezgalan04@gmail.com>
2025-07-22 03:11:40 +0000

    Created the IPv4 header

--- src/ip/ipv4.h
+++ src/ip/ipv4.h
@@ -1,1 +1,26 @@
- 
+#ifndef IPv4_H
+#define IPv4_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+
+typedef struct
+{
+    uint8_t version_ihl; // Version (4 bits) + header length (4 bits)
+    uint8_t tos;
+    uint8_t total_length;
+    uint8_t identification;
+    uint16_t flags_fragment_offset;
+    uint8_t ttl;
+    uint8_t protocol;
+    uint16_t header_checksum;
+    uint8_t src_ip[4];
+    uint8_t src_dst[4];
+} ipv4_header;
+
+void ipv4_handle_packet();
+void ipv4_checksum();
+
+
+#endif
