b4a5088ec858365a245f5cb4f75a1001a5eff27a
Roberto Ramirez <robertoramirezgalan04@gmail.com>
2025-07-22 02:58:11 +0000

    attempted to filter out IPv6 packets

--- src/ethernet/ethernet.c
+++ src/ethernet/ethernet.c
@@ -49,8 +49,7 @@ void ethernet_send(const uint8_t *dst_mac, uint16_t ethertype, const uint8_t *pa
 }
 
 void ethernet_receive(const uint8_t *frame, size_t len) {
-    if (len < sizeof(ethernet_header_t))
-    {
+    if (len < sizeof(ethernet_header_t)) {
         printf("Frame too short to be a valid Ethernet Frame\n");
         return;
     }
@@ -61,34 +60,33 @@ void ethernet_receive(const uint8_t *frame, size_t len) {
     uint16_t ethertype = ntohs(hdr->ethertype);
 
     printf("Source MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
-              hdr -> src_mac[0], hdr -> src_mac[1], hdr -> src_mac[2],
-              hdr -> src_mac[3], hdr -> src_mac[4], hdr -> src_mac[5]);
+           hdr->src_mac[0], hdr->src_mac[1], hdr->src_mac[2],
+           hdr->src_mac[3], hdr->src_mac[4], hdr->src_mac[5]);
+
     printf("Destination MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
-              hdr -> dst_mac[0], hdr -> dst_mac[1], hdr -> dst_mac[2],
-              hdr -> dst_mac[3], hdr -> dst_mac[4], hdr -> dst_mac[5]);
+           hdr->dst_mac[0], hdr->dst_mac[1], hdr->dst_mac[2],
+           hdr->dst_mac[3], hdr->dst_mac[4], hdr->dst_mac[5]);
 
     printf("Ethertype: 0x%04x\n", ethertype);
 
-    switch (ethertype)
-    {
-        case 0x0806: // ARP
-            printf("ARP request received\n");
-            arp_handle_packet(payload, payload_len);
-            break;
-        case 0x0800: // IPv4
-            printf("IPv4 packet received (not handled yet)\n");
-            // TODO ipv4_handle_packet(payload, payload_len);
-            break;
+    switch (ethertype) {
+    case 0x0806: // ARP
+        printf("ARP request received\n");
+        arp_handle_packet(payload, payload_len);
+        break;
 
-        case 0x086dd: // IPv6
-            printf("IPv6 packet received (not handled yet)\n");
-            // TODO implement an IPv6 handler
-            break;
+    case 0x0800: // IPv4
+        printf("IPv4 packet received (not handled yet)\n");
+        // TODO: Call your IPv4 handler here.
+        // ipv4_handle_packet(payload, payload_len);
+        break;
 
+    case 0x86DD: // IPv6
+        printf("Skipping IPv6 packet\n");
+        break;
 
-        default:
-            printf("Unknown ethertype: 0x%04x\n", ethertype);
-
+    default:
+        printf("Unknown ethertype: 0x%04x (skipping)\n", ethertype);
+        break;
     }
-
 }
